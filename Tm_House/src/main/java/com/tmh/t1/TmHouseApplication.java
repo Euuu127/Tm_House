9037b077320bff533ea7b3455b71110ec74ac08c
Euuu127 <mak12cht7@gmail.com>
2021-05-19 22:39:19 +0900

    start

---
+++ Tm_House/src/main/java/com/tmh/t1/TmHouseApplication.java
@@ -0,0 +1,13 @@
+package com.tmh.t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmHouseApplication.class, args);
+	}
+
+}
